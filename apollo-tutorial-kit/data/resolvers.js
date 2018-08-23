b04833ee06d1857dae28a138f6853ebf6819476f
Zygrade <hsenday.6991@gmail.com>
2018-08-23 21:45:09 +0530

    init

---
+++ apollo-tutorial-kit/data/resolvers.js
@@ -0,0 +1,33 @@
+import { Author, View , FortuneCookie, Post } from './connectors.js';
+
+const resolvers = {
+    Query : {
+      author: (_,args) => {
+        return Author.find({ where : args });
+      },
+      allAuthors : (_,args) => {
+        return Author.findAll();
+      },
+      getFortuneCookies : (_,args) => {
+        return FortuneCookie.getOne();
+      }
+    },
+
+    Author : {
+      posts(author) {
+        return author.getPosts();
+      }
+    },
+
+    Post : {
+      author(post) {
+        return post.getAuthor();
+      },
+
+      views(post) {
+        return View.findOne({ postId : post.id }).then(view => view.views);
+      }
+    }
+};
+
+export default resolvers;
---
+++ apollo-tutorial-kit/data/schema.js
@@ -0,0 +1,37 @@
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+import resolvers from './resolvers.js';
+//import mocks from './mocks';
+
+const typeDefs = `
+
+type Query
+{
+   author(firstName : String,lastName : String) : Author
+   allAuthors : [Author]
+   getFortuneCookies : String
+}
+
+type Author
+{
+    id : Int,
+    firstName : String,
+    lastName : String,
+    posts : [Post]
+}
+
+type Post
+{
+    id : Int,
+    text : String,
+    title : String,
+    views : Int,
+    author : Author
+}
+
+`;
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+//addMockFunctionsToSchema({ schema, mocks });
+
+export default schema;
